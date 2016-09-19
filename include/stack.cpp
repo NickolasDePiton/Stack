4b5cbeb3f42788b87475ee5badc233f81bb948df
NickolasDePiton <crysis4997@ya.ru>
2016-09-19 07:21:45 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -19,16 +19,17 @@ private:
 	T * array_;
 	size_t array_size_;
 	size_t count_;
+	T* copy_mas(const T *, size_t, size_t);
 };
 
 template<typename T>
-stack<T>& stack<T>::operator=(const stack& b){
+stack<T>& stack<T>::operator=(const stack& b)
+{
 	if (this != &b){
 		delete[] array_;
 		count_ = b.count_;
 		array_size_ = b.array_size_;
-		array_ = new T[array_size_];
-		copy(b.array_, b.array_ + count_, array_);
+		array_ = copy_mas(b.array_, count_, array_size_);
 	}
 	return *this;
 }
@@ -37,10 +38,10 @@ template <typename T>
 stack<T>::stack() : array_(nullptr), array_size_(0), count_(0){};
 
 template <typename T>
-stack<T>::stack(const stack& x) : array_size_(x.array_size_), count_(x.count_){
-	array_ = new T[array_size_];
-	copy(x.array_, x.array_ + count_, array_);
-}
+stack<T>::stack(const stack& x) : array_size_(x.array_size_), count_(x.count_)
+{
+	array_ = copy_mas(x.array_, count_, array_size_);
+	}
 
 template <typename T>
 stack<T>::~stack(){
@@ -48,30 +49,38 @@ stack<T>::~stack(){
 }
 
 template <typename T>
-size_t stack<T>::count() const{ return count_; }
+size_t stack<T>::count() const
+{ 
+	return count_; 
+	
+}
 
-template <typename T>
-void stack<T>::push(T const &a){
-	if (array_ == nullptr){
-		array_ = new T[1]; array_[0] = a;
-		count_++; array_size_++;
-	}
-	else{
-		if (count_ == array_size_){
-			T *p = array_;
-			array_ = new T[array_size_ * 2];
-			copy(p, p + count_, array_);
-			delete[]p;
-			array_size_ *= 2;
-		}
-		array_[count_] = a;
-		count_++;
-	}
+template<typename T>
+T* stack<T>::copy_mas(const T *p1, size_t c, size_t s)
+{
+	T *p2 = new T[s];
+	copy(p1, p1 + c, p2);
+	return p2;
 }
 
 template <typename T>
-T stack<T>::pop(){
-	if (count_> 0) {
+void stack<T>::push(T const &a)
+{
+	if (count_ == array_size_){
+		T *p = array_;
+		array_ = copy_mas(p, count_, array_size_ * 2+(count_==0));
+		if(p!=nullptr) delete[]p;
+		array_size_ =array_size_*2+(count_==0);
+	}
+	array_[count_] = a;
+	count_++;
+}
+
+template <typename T>
+T stack<T>::pop()
+{
+	if (count_> 0) 
+	{
 		count_--;
 		return array_[count_];
 	}
